e246238167769f365d9f8b0b67a596d618303260
Elliot-Clark <etcclark@gmail.com>
2021-08-06 00:31:59 -0700

    Math Method Adjustment for max limit

--- src/App.js
+++ src/App.js
@@ -23,11 +23,18 @@ class App extends Component {
   main = () => {
     console.log(this.state.outputs);
     if (this.state.inputNumber === 1) {
+      //When the sequence eventually reduces down to 1, the recursive process ends.
+      //In the event you discover a number that doesn't reduce down to the 4,2,1 loop, contact your local mathematician. They should be very interested to hear about it.
       this.state.outputs.push(1);
       return
     }
+    if (this.state.outputs.length > 25) {
+      //While not infinite, some numbers can have a sequence far to long to display so we limit it to 25 before ending.
+      (console.log("Sequence to long"));
+      return
+    }
     if (this.state.inputNumber % 2 > 0) {
-      //Odd Number
+      //Odd Number. Divide the number by 2 and run method again.
       this.setState({
         inputNumber: (this.state.inputNumber * 3 + 1),
       }, () => {
@@ -36,7 +43,7 @@ class App extends Component {
       });
     }
     if (this.state.inputNumber % 2 === 0) {
-      //Even Number
+      //Even Number. Multiply the number by 3 and add 1 then run method again.
       this.setState({
         inputNumber: (this.state.inputNumber / 2),
       }, () => {
