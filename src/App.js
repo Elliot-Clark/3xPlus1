2e4965cdca4c65e091980500a844f263f3aad704
Elliot-Clark <etcclark@gmail.com>
2021-08-13 19:55:49 -0700

    Added Forward/Back buttons and functions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 // Show inputted number 
 // Show highest number
-// Show length of seuqnece
+// Show length of sequences
+// Next and Previous buttons for scrolling through longer sequences 
 // Information
 
 class App extends Component {
@@ -13,12 +14,21 @@ class App extends Component {
     inputNumber: '',
     numberCollection : [],
     idle: true,
+    aboveMaxLength: false,
+    previousNumbers: []
   }
 
-  init = () => {
-    let userInputtedNumber = parseInt(document.getElementsByClassName("inputNumber")[0].value);
+  init = (ele) => {
+    let userInputtedNumber;
+    if (ele) {
+      userInputtedNumber = ele;
+    } else {
+      userInputtedNumber = parseInt(document.getElementsByClassName("inputNumber")[0].value);
+      this.setState({ previousNumbers: [] });
+    }
     this.setState({ 
       inputNumber: userInputtedNumber,
+      aboveMaxLength: false,
       idle: false
     }, () => {
       this.mainComputation();
@@ -37,14 +47,18 @@ class App extends Component {
         //In the event you discover a number that doesn't reduce down to the 4,2,1 loop, contact your local mathematician. They should be very interested to hear about it.
         return
       }
-      //While not infinite, some numbers can have a sequence far to long to display so we limit it to 30 before ending.
-      if (count % 2 > 0 && outputs.length < 30) {
+      //While not infinite, some numbers can have a sequence far to long to display so we limit it to a set number before ending.
+      if (outputs.length > 32) {
+        this.setState({ aboveMaxLength: true});
+        return
+      }
+      if (count % 2 > 0) {
         //Odd Number. Multiply the number by 3 and add 1 then run method again.
         count = count * 3 + 1;
         outputs.push(count);
           calculate();
       }
-      if (count % 2 === 0 && outputs.length < 30) {
+      if (count % 2 === 0) {
         //Even Number. Divide the number by 2 and run method again.
         count = count / 2;
         outputs.push(count);
@@ -53,6 +67,26 @@ class App extends Component {
     }
     calculate();
     this.setState({numberCollection: outputs});
+  }
+
+  nextButton = () => {
+    console.log("ewafe");
+    this.state.previousNumbers.push(this.state.numberCollection[0]);
+    this.setState({
+      inputNumber: this.state.numberCollection[this.state.numberCollection.length - 1],
+    }, () => {
+      this.init(this.state.inputNumber);
+    });
+
+  }
+
+  backButton = () => {
+    this.setState({
+      numberCollection: []
+    }, () => {
+      this.init(this.state.previousNumbers[this.state.previousNumbers.length - 1]);
+      this.state.previousNumbers.pop();
+    });
   }
 
   componentDidMount() {
@@ -65,6 +99,7 @@ class App extends Component {
         document.getElementsByClassName('inputNumber')[0].value = '';
       }
     });
+
     // setInterval(() =>{
     //   if (this.state.idle) {
     //     let randomNumber = Math.floor(Math.random() * 999 + 10);
@@ -80,29 +115,36 @@ class App extends Component {
   }
 
   render() {
- 
-    // let items = []
-    // for (const [index, value] of this.state.numberCollection.entries()) {
-    //   items.push(<li key={index}>{value}</li>)
-    // }
 
     return (
       <div className="App">
 
         <div id="sidebar">
-          <div id="logo">Logo Placeholder</div>
+          <div id="logo">3x + 1</div>
           <div id="stats"></div>
             
         </div>
-        <div id="leftWrapper">
+        <div id="rightWrapper">
           <div id="inputWrapper">
-            <input className="inputNumber" title="Input any positive integer and press Enter to run"></input>
+            <input className="inputNumber" title="Input any positive integer and press Enter to run" placeholder="Enter..."></input>
           </div>
-          <Canvas 
-            numberCollection = {this.state.numberCollection}
-          />
+          <div id="graphWrapper">
+            <div id="forwardBack">
+            {this.state.previousNumbers.length ? (
+                <button id="back" onClick={this.backButton}> &#60; </button>
+              ) : ('')}
+              
+              {this.state.aboveMaxLength ? (
+                <button id="forward" onClick={this.nextButton}> &#62; </button>
+              ) : ('')}
+
+            </div>
+            <Canvas 
+              numberCollection = {this.state.numberCollection}
+            />
+          </div>
           <div id="infoText">
-            <p>ffakwlfl fewlfnewa fwenaklewnfl e</p>
+            <p>The Collatz conjecture is a conjecture in mathematics that concerns sequences defined as follows: start with any positive integer <i>n</i>. Then each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term. If the previous term is odd, the next term is 3 times the previous term plus 1. The conjecture is that no matter what value of n, the sequence will always reach 1.</p>
           </div>
         </div>
 
--- src/Canvas.js
+++ src/Canvas.js
@@ -17,33 +17,28 @@ const Canvas = (props) => {
     const draw = ctx => {
         ctx.clearRect(-100, 0, width + 100, -height);
 
-        ctx.setTransform(1, 0, 0, 1, width * .1 , height);
-        ctx.beginPath();
-        ctx.strokeStyle = '#383f41'; // Horizontal Graph Line
-        ctx.moveTo(0, 0);
-        ctx.lineWidth = 2;
-        ctx.lineTo(width, 0);
-        ctx.stroke();
+        ctx.setTransform(1, 0, 0, 1, 0 , height);
 
         ctx.beginPath();
         ctx.strokeStyle = '#383f41'; // Vertical Graph Line
         ctx.moveTo(0, 0);
+        ctx.lineWidth = 2;
         ctx.lineTo(0, -height);
         ctx.stroke();
 
-        let x = 0;
-        let y = 0;
+        let x = 5;
+        let y = 5;
         let xArray = [];
         let yArray = [];
 
         for (let i = 0; i < arrLength; i++) {
             ctx.beginPath(); //Graphs the lines from one number to the next
-            ctx.moveTo(x, y);
+            ctx.moveTo(x -5, y -5);
             ctx.strokeStyle = "#383f41";
-            x = (i+1)/(arrLength) * width * .85;
-            xArray.push(x);
+            x = (i+1)/(arrLength) * width * .95;
+            xArray.push(x -5);
             y = array[i]/arrLargest * -height * .85;
-            yArray.push(y);
+            yArray.push(y -5);
             ctx.lineTo(x, y)
             ctx.stroke();
         }
@@ -54,7 +49,7 @@ const Canvas = (props) => {
             ctx.strokeStyle = "#383f41"; 
 
             //Sets the size of each circle based on how many digits in each number for better fitting sizes.
-            let circleSize = array[i].toString().split('').length * 5 + 8;
+            let circleSize = array[i].toString().split('').length * 6 + 12;
 
             ctx.arc(xArray[i], yArray[i] - 8, circleSize, 0, 2 * Math.PI);
             ctx.stroke();
@@ -63,7 +58,7 @@ const Canvas = (props) => {
             ctx.fill();
             ctx.font = "bold 28px Arial";
             ctx.textAlign="center";
-            ctx.fillStyle = 'yellow';
+            ctx.fillStyle = 'red';
             ctx.fillText(array[i], xArray[i], yArray[i]);
         }
     }
