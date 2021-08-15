d463055c4ff39de67e0ef117b10acd39ab84c54d
Elliot-Clark <etcclark@gmail.com>
2021-08-15 01:06:00 -0700

    InfoText area designed

--- src/App.js
+++ src/App.js
@@ -147,7 +147,13 @@ class App extends Component {
             />
           </div>
           <div id="infoText">
-            <p>The Collatz conjecture is a conjecture in mathematics that concerns sequences defined as follows: start with any positive integer <i>n</i>. Then each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term. If the previous term is odd, the next term is 3 times the previous term plus 1. The conjecture is that no matter what value of n, the sequence will always reach 1.</p>
+            <p>
+              The Collatz conjecture is a conjecture in mathematics that concerns sequences defined as follows: start with any positive integer <i>n</i>. 
+              Then each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term. 
+              If the previous term is odd, the next term is 3 times the previous term plus 1. 
+              The conjecture is that no matter what value of n, the sequence will always reach 1.
+
+            </p>
           </div>
         </div>
 
